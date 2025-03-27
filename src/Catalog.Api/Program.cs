a3ccf5c049656bd2457e5067541d6c10465a3b6c
semenyaka08 <Serh.semka@gmail.com>
2025-03-27 18:14:44 +0200

    Add Docker Compose setup for Catalog.Api and PostgreSQL

--- src/Catalog.Api/Program.cs
+++ src/Catalog.Api/Program.cs
@@ -4,7 +4,9 @@ using Carter;
 using Catalog.Api.Data;
 using Catalog.Api.Middlewares;
 using FluentValidation;
+using HealthChecks.UI.Client;
 using Marten;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,11 +33,20 @@ builder.Services.AddScoped<ExceptionHandlingMiddleware>();
 if (builder.Environment.IsDevelopment())
     builder.Services.InitializeMartenWith<CatalogInitialData>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 //Configure the HTTP request pipeline
 app.MapCarter();
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();

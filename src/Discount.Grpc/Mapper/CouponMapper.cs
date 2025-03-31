01bc0aa530cc037b0b8379b71343c3192749db35
semenyaka08 <Serh.semka@gmail.com>
2025-03-31 15:39:55 +0300

    complete DiscountService with CRUD operations and logging

---
+++ src/Discount.Grpc/Mapper/CouponMapper.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Grpc.Models;
+
+namespace Discount.Grpc.Mapper;
+
+public static class CouponMapper
+{
+    public static CouponModel ToResponse(this Coupon coupon) => new()
+    {
+        Id = coupon.Id,
+        ProductName = coupon.ProductName,
+        Description = coupon.Description,
+        Amount = coupon.Amount
+    };
+
+    public static Coupon ToModel(this CouponModel model) => new()
+    {
+        Id = model.Id,
+        ProductName = model.ProductName,
+        Description = model.Description,
+        Amount = model.Amount
+    };
+}

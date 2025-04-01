71c7dba063d7b433afc54f56d5f303b060f2c052
semenyaka08 <Serh.semka@gmail.com>
2025-04-01 19:31:18 +0300

    Develop Domain modeling following DDD principles for Ordering microservice

---
+++ src/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+    {
+        Id = Guid.NewGuid();
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public Guid OrderId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
+}
---
+++ src/Ordering.Domain/ValueObjects/Payment.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Domain.ValueObjects;
+
+public record Payment
+{
+    public string? CardName { get; } = default!;
+    public string CardNumber { get; } = default!;
+    public string Expiration { get; } = default!;
+    public string Cvv { get; } = default!;
+    public int PaymentMethod { get; } = default!;
+
+    protected Payment()
+    {
+    }
+
+    private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        CardName = cardName;
+        CardNumber = cardNumber;
+        Expiration = expiration;
+        Cvv = cvv;
+        PaymentMethod = paymentMethod;
+    }
+};
